96cb08abe054b2ceeb6fb66af1f141a13c4dfb0c
AnzelikaBula <125495176+AnzelikaBula@users.noreply.github.com>
2023-03-19 22:56:06 +0000

    221RDC032 4.uzd Anzelika Bula

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,59 @@
-# python3
+def heapify(data, j, i, swaps):
+    
+    
+    smallest = i 
+    right = 2 * i + 2     
+    left = 2 * i + 1   
+     
+       
+
+    if left < j and data[i] > data[left]:
+        smallest = left
+
+    if right < j and data[smallest] > data[right]:
+        smallest = right
+
+    if smallest != i:
+        data[i], data[smallest] = data[smallest], data[i]
+        swaps.append((i, smallest))
+        heapify(data, j, smallest, swaps)
 
 
-def build_heap(data):
+def build_heap(data, j):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+    for i in range(j // 2, -1, -1):
+        heapify(data, j, i, swaps)
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    try:
+        text = input("Enter I or F:  ")
+        
+        if text.startswith('I'):
+            n = int(input("digit: "))
+            data = list(map(int, input().split()))
+        elif text.startswith('F'):
+            filename = "tests/" + input("Fails: ")
+            with open(filename, "r") as file:
+                n = int(file.readline())
+                data = list(map(int, file.readline().split()))
 
+        assert len(data) == n
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        swaps = build_heap(data, n)
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+        print(len(swaps))
+        
+        for a, b in swaps:
+            print(a, b)
+            
+    except Exception as e:
+        
+        print(f"Error: {e}")
+        return
 
 if __name__ == "__main__":
     main()
